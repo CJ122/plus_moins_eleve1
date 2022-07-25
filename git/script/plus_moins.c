8b7f1a28001d6d0278605c1209f1c31147b9c5a0
CJ122 <chavishkajotee12@gmail.com>
2022-07-25 21:12:08 +0400

    version finale

--- git/script/plus_moins.c
+++ git/script/plus_moins.c
@@ -3,65 +3,31 @@
 #include <math.h>
 #include <stdlib.h>
 
-int choix_joueur, valeur_a_trouver, r, mod_diff, ess;
-
+int choix_joueur, valeur_a_trouver1, valeur_a_trouver2, r, ess, num_joueur, num_tour,j, score_joueur1, score_joueur2;
 //Fonction pour générer un nombre aléatoire entre 0 et 100
 void plus_moins(int r)
 {
   srand(time(NULL));
-  valeur_a_trouver = rand() % r;
-    
-  scanf("%d", &mod_diff);
- 
+  valeur_a_trouver1 = rand() % r;
+  valeur_a_trouver2 = rand() % r;
   //On utilise le do-while loop tant que l'utilisateur essaye de deviner la bonne valeur.
-  do{
-    //la mode de difficulté facile
-    if (mod_diff == 1)
-    {
-      printf ("Mode Facile--->Vous avez un nombre d'essaie sans limites.\n");
-      printf ("Deviner la bonne valeur: "); 
+   do{
       
-      for(;;){
-        scanf("%d", &choix_joueur);
-        //Quand l'utilisateur choisi une valeur moins que la bonne valeur
-        if (choix_joueur < valeur_a_trouver)
-        {
-          printf("La bonne valeur est plus!\n");
-          printf("Veuillez choisir de nouveau une valeur: ");
-          
-        }
-  
-      //Quand l'utilisateur choisi une valeur plus grande que la bonne valeur  
-        else if (choix_joueur > valeur_a_trouver)
-        { 
-          printf("La bonne valeur est moins!\n");
-          printf("Veuillez choisir de nouveau une valeur: ");
-          
-        }
-        //Quand l'utilisateur a choisi la bonne valeur
-        else 
-        {
-          printf ("Bingo!\nVous avez eu la bonne valeur!");
-          break;
-        }
-      }
-    }
-  
-  
-
-    
-    //la mode de difficulté moyenne
-    if (mod_diff == 2)
-    {
-      printf ("Mode Moyen--->Vous avez un nombre d'essaie de 25.\n");
+      printf ("|--------------Tour %d--------------|\n",num_tour);
+      printf("Vous etes joueur 1 ou 2? Entrez 1 pour joueur1 et 2 pour joueur2: ");
+      scanf("%d", &num_joueur);
+      printf ("Bonjour joueur %d, c'est votre tour.\n", num_joueur);
+      printf("Le second joueur doit entrer le nombre d'essaie pour vous: ");
+      scanf ("%d",&ess);
       printf ("Deviner la bonne valeur: "); 
-      ess=25;
-      for (int i = 25; i > 0; i--)
+      if (num_joueur == 1)
+      {
+      for (int i = ess ; i > 0; i--)
       {
         
         scanf("%d", &choix_joueur);
         //Quand l'utilisateur choisi une valeur moins que la bonne valeur
-        if (choix_joueur < valeur_a_trouver)
+        if (choix_joueur < valeur_a_trouver1)
         {
           printf("La bonne valeur est plus!\n");
           printf("Veuillez choisir de nouveau une valeur: ");
@@ -69,7 +35,7 @@ void plus_moins(int r)
         }
   
       //Quand l'utilisateur choisi une valeur plus grande que la bonne valeur  
-        else if (choix_joueur > valeur_a_trouver)
+        else if (choix_joueur > valeur_a_trouver1)
         { 
           printf("La bonne valeur est moins!\n");
           printf("Veuillez choisir de nouveau une valeur: ");
@@ -78,31 +44,29 @@ void plus_moins(int r)
         //Quand l'utilisateur a choisi la bonne valeur
         else 
         {
-          printf ("Bingo!\nVous avez eu la bonne valeur!");
+          printf ("Bingo!\nVous avez eu la bonne valeur joueur %d!\n", num_joueur);
+          num_tour--;
+          score_joueur1=ess;
           break;
         }
           
         //Si nombre d'essaies est au Maximum.
-        if (i == 0)
+        if (ess == 0)
         {
           printf("Maximum nombre d'essaies. Vous avez perdu.");
+          num_tour--;
+          break;
         }
       }
-    }
-
-    
-    //la mode de difficulté difficile
-    if (mod_diff == 3)
-    {
-      printf ("Mode difficile--->Vous avez un nombre d'essaie de 10.\n");
-      printf ("Deviner la bonne valeur: "); 
-      ess=10;
-      for (int i = 10; i > 0; i--)
+      }
+      else if (num_joueur == 2)
+      {
+      for (int i = ess ; i > 0; i--)
       {
         
         scanf("%d", &choix_joueur);
         //Quand l'utilisateur choisi une valeur moins que la bonne valeur
-        if (choix_joueur < valeur_a_trouver)
+        if (choix_joueur < valeur_a_trouver2)
         {
           printf("La bonne valeur est plus!\n");
           printf("Veuillez choisir de nouveau une valeur: ");
@@ -110,7 +74,7 @@ void plus_moins(int r)
         }
   
       //Quand l'utilisateur choisi une valeur plus grande que la bonne valeur  
-        else if (choix_joueur > valeur_a_trouver)
+        else if (choix_joueur > valeur_a_trouver2)
         { 
           printf("La bonne valeur est moins!\n");
           printf("Veuillez choisir de nouveau une valeur: ");
@@ -119,32 +83,51 @@ void plus_moins(int r)
         //Quand l'utilisateur a choisi la bonne valeur
         else 
         {
-          printf ("Bingo!\nVous avez eu la bonne valeur!");
+          printf ("Bingo!\nVous avez eu la bonne valeur joueur %d!\n", num_joueur);
+          num_tour--;
+          score_joueur2=ess;
           break;
         }
           
         //Si nombre d'essaies est au Maximum.
-        if (i == 0)
+        if (ess == 0)
         {
           printf("Maximum nombre d'essaies. Vous avez perdu.");
+          num_tour--;
+          break;
         }
       }
-    }
-  }while(choix_joueur != valeur_a_trouver);
+      }
     
+    }while(num_tour != 0);
+    //Afficher les scores des deux joueurs et le gagnant.
+    printf("::::::::::::::::::Scores:::::::::::::::::::::::\njoueur1--->%d\tjoueur2--->%d\n",score_joueur1,score_joueur2);
+    if (score_joueur1 > score_joueur2)
+    {
+      printf("Bravo! Le gagnant est joueur 1!\n");
+    }
+    else
+      printf("Bravo! Le gagnant est joueur 2!\n");
 }
-    
-    
     
 
 int main()
 {
-    int r = 100;
-    printf("Bienvenue au jeu plus ou moins!\n");
-    printf("Vous avez 3 modes de difficulte: 1 pour facile\t2 pour moyen\t 3 pour difficile\n");
-    printf("Entrez le chiffre du mode de difficulté: ");
-     
+  int r = 100;
+  printf ("Bienvenue au jeu plus ou moins!\n");
+  //Pour choisir le nombre de tour.
+  printf ("Veillez entrez le nombre de tour que vous souhaitez jouer: ");
+  scanf ("%d", &num_tour);
+  //For loop tant que le nombre de tour n'est pas 0.
+  //A chaque tour, le joueur qui doit jouer devra entrer son numero(1 ou 2).
+  for (int j = 1; j= num_tour; j++)
+  { 
     //Appel de la fonction plus_moins
     plus_moins(r);
     return 0;
+  }
 }
+
+
+        
+   
